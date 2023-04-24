b032b8bfcaa8f8ecb3b8ba1e72c32a5f7f08b3c1
Jakub Znyk <jakubznyk@protonmail.com>
2023-04-24 13:35:54 +0200

    lab04: Add images to the Dice Roller app

--- src/lab04/Add_a_button_to_an_app/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ src/lab04/Add_a_button_to_an_app/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.happybirthday
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -12,13 +12,32 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDice() }
+
+        // Do a dice roll when the app starts
+        rollDice()
     }
 
     private fun rollDice() {
+        // Create dice object and roll
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+
+        // Find dice imageView
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        // Change image to rolled dice
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
+
     }
 }
 
