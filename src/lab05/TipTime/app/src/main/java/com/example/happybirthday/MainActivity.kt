aaaa84bdcd251724100beccbf82e1e64eba40561
Jakub Znyk <jakubznyk@protonmail.com>
2023-04-24 16:41:12 +0200

    lab05: material components

--- src/lab05/TipTime/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ src/lab05/TipTime/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -18,33 +18,33 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calculateTip() {
-        // Get written cost
-        val cost = binding.costOfService.text.toString().toDoubleOrNull()
-
-        if (cost == null) {
-            binding.tipResult.text = ""
-            return
-        }
-
-        // Get chosen tip percentage
-        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
-            R.id.option_twenty_percent -> 0.20
-            R.id.option_eighteen_percent -> 0.18
-            else -> 0.15
-        }
-
-        // Calculate tip
-        var tip = tipPercentage * cost
-
-        // Round tip if switch is checked
-        if(binding.roundUpSwitch.isChecked) {
-            tip = kotlin.math.ceil(tip)
-        }
-
-        // Format tip in order to show it in proper currency
-        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
-
-        // Set text in view
-        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+//        // Get written cost
+//        val cost = binding.costOfServiceEditText.text.toString().toDoubleOrNull()
+//
+//        if (cost == null) {
+//            binding.tipResult.text = ""
+//            return
+//        }
+//
+//        // Get chosen tip percentage
+//        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+//            R.id.option_twenty_percent -> 0.20
+//            R.id.option_eighteen_percent -> 0.18
+//            else -> 0.15
+//        }
+//
+//        // Calculate tip
+//        var tip = tipPercentage * cost
+//
+//        // Round tip if switch is checked
+//        if(binding.roundUpSwitch.isChecked) {
+//            tip = kotlin.math.ceil(tip)
+//        }
+//
+//        // Format tip in order to show it in proper currency
+//        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+//
+//        // Set text in view
+//        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
