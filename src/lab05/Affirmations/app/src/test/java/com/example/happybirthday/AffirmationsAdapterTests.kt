b946f64ea97bc15f12f4df832e1475f8e80f92aa
Jakub Znyk <jakubznyk@protonmail.com>
2023-04-27 20:05:51 +0200

    lab05: Test Lists and Adapters

---
+++ src/lab05/Affirmations/app/src/test/java/com/example/happybirthday/AffirmationsAdapterTests.kt
@@ -0,0 +1,22 @@
+package com.example.happybirthday
+
+import android.content.Context
+import com.example.happybirthday.adapter.ItemAdapter
+import com.example.happybirthday.model.Affirmation
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import org.mockito.Mockito.mock
+
+class AffirmationsAdapterTests {
+    private val context = mock(Context::class.java)
+
+    @Test
+    fun adapter_size() {
+        val data = listOf(
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation2, R.drawable.image2)
+        )
+        val adapter = ItemAdapter(context, data)
+        assertEquals("ItemAdapter is not the correct size", data.size, adapter.itemCount)
+    }
+}
