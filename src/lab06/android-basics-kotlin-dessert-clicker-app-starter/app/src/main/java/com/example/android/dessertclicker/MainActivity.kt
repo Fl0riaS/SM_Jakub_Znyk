8806f9b7c8220bb0bec70db93db161560babf829
Jakub Znyk <zj49454@zut.edu.pl>
2023-04-28 16:58:25 +0200

    end of lab07

--- src/lab06/android-basics-kotlin-dessert-clicker-app-starter/app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ src/lab06/android-basics-kotlin-dessert-clicker-app-starter/app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -28,6 +28,9 @@ import androidx.databinding.DataBindingUtil
 import com.example.android.dessertclicker.databinding.ActivityMainBinding
 
 const val TAG = "MainActivity"
+const val KEY_REVENUE = "revenue_key"
+const val KEY_DESSERT_SOLD = "dessert_sold_key"
+
 class MainActivity : AppCompatActivity() {
 
     private var revenue = 0
@@ -68,6 +71,11 @@ class MainActivity : AppCompatActivity() {
         Log.d("MainActivity", "onCreate Called")
         // Use Data Binding to get reference to the views
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        if (savedInstanceState != null) {
+            revenue = savedInstanceState.getInt(KEY_REVENUE, 0)
+            dessertsSold = savedInstanceState.getInt(KEY_DESSERT_SOLD, 0)
+            showCurrentDessert()
+        }
 
         binding.dessertButton.setOnClickListener {
             onDessertClicked()
@@ -110,6 +118,16 @@ class MainActivity : AppCompatActivity() {
         super.onRestart()
         Log.d(TAG, "onRestart Called")
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        Log.d(TAG, "onSaveInstanceState Called")
+
+        outState.putInt(KEY_REVENUE, revenue)
+        outState.putInt(KEY_DESSERT_SOLD, dessertsSold)
+    }
+
 
     /**
      * Updates the score when the dessert is clicked. Possibly shows a new dessert.
