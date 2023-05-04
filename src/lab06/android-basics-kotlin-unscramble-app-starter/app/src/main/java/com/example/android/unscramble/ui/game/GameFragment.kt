721c5285c66c749c79f9397748b4601301da2380
Jakub Znyk <jakubznyk@protonmail.com>
2023-05-04 20:48:39 +0200

    lab06: Implement the Skip button

--- src/lab06/android-basics-kotlin-unscramble-app-starter/app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ src/lab06/android-basics-kotlin-unscramble-app-starter/app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -86,6 +86,12 @@ class GameFragment : Fragment() {
      * Increases the word count.
      */
     private fun onSkipWord() {
+        if (viewModel.nextWord()) {
+            setErrorTextField(false)
+            updateNextWordOnScreen()
+        } else {
+            showFinalScoreDialog()
+        }
     }
 
     /*
