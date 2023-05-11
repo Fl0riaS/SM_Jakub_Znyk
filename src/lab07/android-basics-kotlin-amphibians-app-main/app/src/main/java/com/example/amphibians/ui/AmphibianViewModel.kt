6b4dde38e9ef4d7d32729c07486e4b6b38f37fb9
Jakub Znyk <jakubznyk@protonmail.com>
2023-05-11 23:45:27 +0200

    lab07: Project: Amphibians app

--- src/lab07/android-basics-kotlin-amphibians-app-main/app/src/main/java/com/example/amphibians/ui/AmphibianViewModel.kt
+++ src/lab07/android-basics-kotlin-amphibians-app-main/app/src/main/java/com/example/amphibians/ui/AmphibianViewModel.kt
@@ -42,7 +42,7 @@ class AmphibianViewModel : ViewModel() {
 
     // TODO: Create a function that gets a list of amphibians from the api service and sets the
     //  status via a Coroutine
-    fun getAmphibianList(){
+    fun getAmphibiansList(){
         viewModelScope.launch {
             _status.value = AmphibianApiStatus.LOADING
             try {
